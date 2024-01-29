1833ab1638f073ffe9df575acec11e8e54454f77
Nimra Amjad <74602690+Nimra-Amjad@users.noreply.github.com>
2024-01-29 20:42:37 +0500

    digital signature

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:signature/signature.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/widgets.dart' as p;
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,12 +18,34 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Uint8List? exportedImage;
+  double bottom = 0.0;
+  double right = 0.0;
 
   SignatureController controller = SignatureController(
     penStrokeWidth: 3,
     penColor: Colors.black,
     exportBackgroundColor: Colors.grey,
   );
+
+  Future<void> makePDF() async {
+    final pdf = p.Document();
+    pdf.addPage(p.Page(build: (context) {
+      return p.Column(children: [p.Text("Nimra Amjad")]);
+    }));
+    Directory? downloadsDirectory = await getExternalStorageDirectory();
+    String path = '${downloadsDirectory!.path}/test12345.pdf';
+    final file = File(path);
+    await file.writeAsBytes(await pdf.save());
+    print("Path " + path);
+    Fluttertoast.showToast(
+        msg: "Downloaded",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
 
   void signature() async {
     showDialog(
@@ -42,6 +70,12 @@ class _HomePageState extends State<HomePage> {
           actions: <Widget>[
             Row(
               children: [
+                TextButton(
+                  child: const Text('Clear'),
+                  onPressed: () async {
+                    controller.clear();
+                  },
+                ),
                 TextButton(
                   child: const Text('Save'),
                   onPressed: () async {
@@ -70,37 +104,62 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 700,
-                child: SfPdfViewer.asset('assets/Nimra Amjad.pdf'),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      signature();
-                    },
-                    child: const Text("Signature"),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  if (exportedImage != null)
-                    Image.memory(
-                      exportedImage!,
-                      width: 80,
-                      height: 100,
-                    )
-                ],
-              )
-            ],
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    SizedBox(
+                      height: 700,
+                      child: SfPdfViewer.asset('assets/Nimra Amjad.pdf'),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    if (exportedImage != null)
+                      Positioned(
+                        bottom: bottom,
+                        right: right,
+                        child: GestureDetector(
+                          onPanUpdate: (details) {
+                            setState(() {
+                              right = max(0, right - details.delta.dx);
+                              bottom = max(0, bottom - details.delta.dy);
+                            });
+                          },
+                          child: Image.memory(
+                            exportedImage!,
+                            width: 80,
+                            height: 60,
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        signature();
+                      },
+                      child: Text(exportedImage != null
+                          ? "Edit Signature"
+                          : "Signature"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        makePDF();
+                      },
+                      child: const Text("Download Pdf"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
